7c2cde7003d83e736002f8cb000e271f9764e2b7
Havi <gouveia.cesar07@gmail.com>
2022-07-29 16:30:52 -0300

    finalização do jogo da  adivinhação

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,11 +1,27 @@
 import random
 
+print("***********************************")
 print("Bem vindo ao jogo de adivinhação!")
+print("***********************************")
 
-numero_secreto = 42
+numero_secreto = round(random.randrange(1,101))
+total_tentativas = 0
 
-total_tentativas = 3
-rodada = 1
+print("Qual nivel de dificuldade ?")
+print("Cadete (1) Espartano (2) Rei Leonidas (3)")
+nivel = int(input("Escolha: "))
+
+if(nivel == 1):
+    total_tentativas = 20
+    print("Vamos lá. Cadete!")
+
+elif(nivel == 2):
+    total_tentativas = 10
+    print("Vamos lá, Espatano!")
+
+else:
+    total_tentativas = 5
+    print("Vamos lá, Meu Rei!")
 
 for rodada in range (1, total_tentativas + 1) :
     print("Tentativa {} de {}".format(rodada, total_tentativas))
@@ -29,5 +45,8 @@ for rodada in range (1, total_tentativas + 1) :
             print("Leonidas: Um pouco menos...")
         elif(menor):
             print("Leonidas: Um pouco mais...")
+
+    if(rodada == total_tentativas):
+        print("THIS IS SPARTA! PERDEU!")
 
 print("Fim de jogo.")
