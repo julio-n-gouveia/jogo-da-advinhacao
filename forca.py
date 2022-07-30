586aca6f825679762b3fa9b1df7e53a23e2b9773
Havi <gouveia.cesar07@gmail.com>
2022-07-29 23:10:07 -0300

    Criação da lógica do jogo da forca

--- forca.py
+++ forca.py
@@ -3,16 +3,24 @@ def jogar():
     print("Jogo da Forca")
     print("************************")
 
-    palavre_secreta = "Banana"
+    palavre_secreta = "banana"
     enforcou = False
     acertou = False
 
 
     while (not enforcou and not acertou):
-        print("jogando")
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+        index = 0
+
+        for letra in palavre_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+
 
 
     print("Fim do jogo.")
-
 if(__name__ == "__main__" ):
     jogar()
